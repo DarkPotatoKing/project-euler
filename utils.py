a5030d3a9f5ad99dc216eb6cf7513bd33de95036
DarkPotatoKing <kyle.rosales97@gmail.com>
2019-06-28 16:39:52 +0800

    Add functions to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
-from math import sqrt
+from math import sqrt, floor, ceil
+from itertools import combinations, permutations, product
+
+def average(ls):
+    return float(sum(ls)) / float(len(ls))
+
+def divide(numerator, denominator):
+    return float(numerator) / float(denominator)
+
+def factorial(n):
+    if n < 0:
+        raise StandardError('Factorial is not defined for negative numbers')
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 def is_prime(n):
     if n < 2:
@@ -9,3 +24,12 @@ def is_prime(n):
             return False
 
     return True
+
+def multiply(a, b):
+    return a * b
+
+def nCr(n, k):
+    if k > n or k < 0:
+        return 0.0
+    else:
+        return divide(factorial(n), factorial(k) * factorial(n - k))
