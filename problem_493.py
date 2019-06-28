7956f1c41e11400afb24c1753098cac8e85f8c3e
DarkPotatoKing <kyle.rosales97@gmail.com>
2019-06-28 16:54:08 +0800

    Add stat solution for problem 493

--- problem_493.py
+++ problem_493.py
@@ -1,15 +1,20 @@
 from utils import *
 
-def main(balls = 70, colors = 7, draws = 20):
-    def count_colors(draw_combination):
-        return len(filter(lambda x: x > 0, draw_combination))
-
+def main(balls = 70, colors = 7, draws = 20, decimal_places = 9):
     balls_per_color = balls / colors
 
-    draw_combinations = filter(lambda x: sum(x) == draws, product(xrange(balls_per_color + 1), repeat=colors))
-    numerator = sum([reduce(multiply, [nCr(balls_per_color, x) for x in c]) * count_colors(c) for c in draw_combinations])
-    denominator = nCr(balls, draws)
-    answer = divide(numerator, denominator)
-    print round(answer, 9)
+    # let p = probability of a red ball existing in a draw
+    # let pc = probability of a red ball NOT existing in a draw
+
+    # out of 60 non-red balls, pick 20
+    # divide by total number of combinations (70 pick 20)
+    pc = divide(nCr(balls - balls_per_color, draws), nCr(balls, draws))
+    p = 1 - pc
+
+    # expected value is 7 * (probability of 1 color existing) = 7p
+    answer = colors * p
+
+    # round to 9 decimal places
+    print round(answer, decimal_places)
 
 main()
